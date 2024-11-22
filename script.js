b9e4db0a3c9cc4eaf3085af3dacd1e46fcdbc84f
samh7 <samh74490@example.com>
2024-11-22 03:35:35 +0300

    Add input event listeners

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const replaceTextButton = document.getElementById("replace-button");
 const replaceFullText = document.getElementById("replace-full-text");
 const replaceTextWord = document.getElementById("replace-text-word");
+const uppercaseInput = document.getElementById("uppercase-input");
+const lowercaseInput = document.getElementById("lowercase-input");
+const capitalizeInput = document.getElementById("capitalize-input");
 
 replaceTextButton.addEventListener("click", function () {
   if (
@@ -13,4 +16,19 @@ replaceTextButton.addEventListener("click", function () {
     );
     replaceFullText.value = replacedText;
   }
+});
+
+uppercaseInput.addEventListener("change", (e) => {
+  e.target.value = `${e.target.value}`.toUpperCase();
+});
+
+lowercaseInput.addEventListener("change", (e) => {
+  e.target.value = `${e.target.value}`.toLowerCase();
+});
+capitalizeInput.addEventListener("change", (e) => {
+  const inputText = `${e.target.value}`;
+
+  if (inputText !== "")
+    e.target.value =
+      inputText.slice(0, 1).toUpperCase + inputText.slice(1, inputText.length);
 });
